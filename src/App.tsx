f3708cfa46d6e2d04145e588e33a3a2ed79148a6
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-17 14:56:42 -0300

    feat(App): import and render InputTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Header } from './components/Header'
+import { InputTask } from './components/InputTask';
 
 function App() {
   return (
     <div className='h-screen bg-gray-dark'>
       <Header />
+      <InputTask />
     </div>
   );
 }
