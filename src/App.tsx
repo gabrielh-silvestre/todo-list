57387747f3d4bb75809a84fe93c3b58c71f6ed0f
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-17 15:15:46 -0300

    feat(App): import and render TaskList

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header'
 import { InputTask } from './components/InputTask';
+import TaskList from './components/TaskList';
 
 function App() {
   return (
     <div className='h-screen bg-gray-dark'>
       <Header />
       <InputTask />
+      <TaskList />
     </div>
   );
 }
