6d37277d741f35041c2377a2164d8c09cf8664eb
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-17 14:26:00 -0300

    build(tailwind): configure colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'gray-dark': '#171717',
+      'gray': '#444444',
+      'gray-light': '#EDEDED',
+      'red': '#DA0037',
+    },
   },
   plugins: [],
 };
