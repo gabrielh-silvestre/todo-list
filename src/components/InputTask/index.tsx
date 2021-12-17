9d679618b86899118a7c9cd00605836064e7e203
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-17 17:43:40 -0300

    feat(InputTask): create tasks using redux

--- src/components/InputTask/index.tsx
+++ src/components/InputTask/index.tsx
@@ -1,13 +1,38 @@
-import { Container, CreateTaskButton, Input } from "./styles";
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { addTask } from '../../slice';
+
+import { Container, CreateTaskButton, Input } from './styles';
 
 export function InputTask() {
+  const [taskTitle, setTaskTitle] = useState('');
+  const dispatch = useDispatch();
+
+  const handleTaskTitle = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskTitle(value);
+  };
+
+  const handleNewTask = () => {
+    dispatch(addTask({ title: taskTitle }));
+  };
+
   return (
     <Container>
       <label htmlFor="inputTask">
-        <Input type="text" name="inputTask" id="inputTask" />
+        <Input
+          type="text"
+          name="inputTask"
+          id="inputTask"
+          onChange={handleTaskTitle}
+        />
       </label>
 
-      <CreateTaskButton type="button">Criar</CreateTaskButton>
+      <CreateTaskButton type="button" onClick={handleNewTask}>
+        Criar
+      </CreateTaskButton>
     </Container>
-  )
+  );
 }
