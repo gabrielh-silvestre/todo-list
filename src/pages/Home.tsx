a9008a583a9a5effe801a48e84ea7f1d15b4026c
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-20 13:46:59 -0300

    refactor(Home): remove Header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,11 @@
-import { Header } from '../components/Header';
 import { InputTask } from '../components/InputTask';
 import TaskList from '../components/TaskList';
 
 export function Home() {
   return (
-    <div className='h-screen bg-gray-dark'>
-      <Header />
+    <>
       <InputTask />
       <TaskList />
-    </div>
+    </>
   );
 }
