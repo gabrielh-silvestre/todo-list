ced170b2e7ea79302e35edb47294c526215ad2f3
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-20 14:58:59 -0300

    feat(pages): create TaskDetail

---
+++ src/pages/TaskDetail/index.tsx
@@ -0,0 +1,10 @@
+import { TaskModal } from '../../components/TaskModal';
+import { Container } from './styles';
+
+export function TaskDetail() {
+  return (
+    <Container>
+      <TaskModal />
+    </Container>
+  );
+}
---
+++ src/pages/TaskDetail/styles.ts
@@ -0,0 +1,7 @@
+import tw from 'tailwind-styled-components';
+
+export const Container = tw.article`
+  container
+
+  mt-8
+`;
