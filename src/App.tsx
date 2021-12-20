c722d4455a0b685dc39c4964ad2ee119878f32b1
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-20 15:05:03 -0300

    refactor(App): encasulate routes in main tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { Route, Switch } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
+import { TaskDetail } from './pages/TaskDetail';
 
 function App() {
   return (
     <div className="h-screen bg-gray-dark">
       <Header />
-      <Switch>
-        <Route path="/" render={Home} />
-      </Switch>
+      <main>
+        <Switch>
+          <Route path="/task" render={TaskDetail} />
+          <Route exact path="/" render={Home} />
+        </Switch>
+      </main>
     </div>
   );
 }
