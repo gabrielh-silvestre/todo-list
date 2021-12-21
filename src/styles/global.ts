5dc2c9e739ded4926b7946d4090ce61d2db89d84
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-12-21 15:12:21 -0300

    style(global): customize React-Modal

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  .react-modal-overlay {
+    background: rgba(0, 0, 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .react-modal-content {
+    width: 100%;
+    max-width: 576px;
+    background: #EDEDED;
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.375rem;
+  }
+`;
